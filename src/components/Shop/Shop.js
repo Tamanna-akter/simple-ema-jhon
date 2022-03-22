80b64fb7c107f462e4aed857af570bb34df992de
Tamanna-akter <atamanna577@gmail.com>
2022-03-22 23:13:33 +0600

    event-handler-added

---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,33 @@
+
+import { useEffect, useState } from 'react';
+import Product from '../Product/Product';
+import './Shop.css'
+const Shop = () => {
+    const [products, setProducts] = useState([]);
+    useEffect(() => {
+        fetch('products.json')
+            .then(res => res.json())
+            .then(data => setProducts(data))
+    }, []);
+    const handleAddtoCart = (product) => {
+        console.log(product);
+    }
+
+    return (
+        <div className='shop-container'>
+            <div className="products-container">
+                {products.map(product =>
+                    <Product key={product.id}
+                        product={product}
+                        handleAddtoCart={handleAddtoCart}
+                    >
+                    </Product>)}
+            </div>
+            <div className="cart-container">
+                <h3>Order summary</h3>
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
